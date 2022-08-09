2ebff061e209b64054fca7957519af8c3c9e5f51
DESKTOP-H3C5UB7\Administrator <xianyue390348@163.com>
2022-08-09 23:23:02 +0800

    update

--- src/index.tsx
+++ src/index.tsx
@@ -2,24 +2,33 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.less";
 import App from "./App";
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-  Navigate
-} from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Planning from "./pages/collaboration/planning/index";
-import 'antd/dist/antd.less'
-
+import "antd/dist/antd.less";
+import { Provider } from "mobx-react";
+import appStore from "./store/appStore";
+import { configure } from "mobx"
 const root = ReactDOM.createRoot(document.getElementById("root")!);
+
+const stores = {appStore}
+configure({enforceActions: 'always'})
 
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route index={false} path="/collaboration/planning" element={<Planning />}></Route>
-        <Route path="*" element={<Navigate replace to="/collaboration/planning" />} />
-      </Routes>
+      <Provider {...stores}>
+        <Routes>
+          <Route
+            index={false}
+            path="/collaboration/planning"
+            element={<Planning />}
+          ></Route>
+          <Route
+            path="*"
+            element={<Navigate replace to="/collaboration/planning" />}
+          />
+        </Routes>
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>
 );
